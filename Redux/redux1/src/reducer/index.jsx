1bf1aa1f7669d44a9596cc305b2a3bd5b7ec015e
Dipesh77Dev <rajpersonal777@gmail.com>
2023-08-01 17:35:51 +0530

    basic startup for redux1

---
+++ Redux/redux1/src/reducer/index.jsx
@@ -0,0 +1,9 @@
+import changeValue from "./action";
+
+import { combineReducers } from "redux";
+
+// const rootReducer = combineReducers({changeValue: changeValue});
+
+const rootReducer = combineReducers({changeValue});
+
+export default rootReducer;
